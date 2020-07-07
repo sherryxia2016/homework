26265500746cc960d52192e42c3b130e9ae6dc8c
diudiu <453509684@qq.com>
2020-07-07 14:50:47 +0800

    changes based on TF 2.0

--- hw1/run_expert.py
+++ hw1/run_expert.py
@@ -11,11 +11,14 @@ Author of this script and included expert policies: Jonathan Ho (hoj@openai.com)
 
 import os
 import pickle
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 import numpy as np
 import tf_util
 import gym
 import load_policy
+tf.disable_v2_behavior()
+
+
 
 def main():
     import argparse
@@ -35,7 +38,7 @@ def main():
     with tf.Session():
         tf_util.initialize()
 
-        import gym
+        # import gym
         env = gym.make(args.envname)
         max_steps = args.max_timesteps or env.spec.timestep_limit
 
@@ -49,7 +52,7 @@ def main():
             totalr = 0.
             steps = 0
             while not done:
-                action = policy_fn(obs[None,:])
+                action = policy_fn(obs[None, :])
                 observations.append(obs)
                 actions.append(action)
                 obs, r, done, _ = env.step(action)
@@ -57,7 +60,8 @@ def main():
                 steps += 1
                 if args.render:
                     env.render()
-                if steps % 100 == 0: print("%i/%i"%(steps, max_steps))
+                if steps % 100 == 0:
+                    print("%i/%i" % (steps, max_steps))
                 if steps >= max_steps:
                     break
             returns.append(totalr)
@@ -66,11 +70,12 @@ def main():
         print('mean return', np.mean(returns))
         print('std of return', np.std(returns))
 
-        expert_data = {'observations': np.array(observations),
-                       'actions': np.array(actions)}
+        # expert_data = {'observations': np.array(observations),
+        #                'actions': np.array(actions)}
+        #
+        # with open(os.path.join('expert_data', args.envname + '.pkl'), 'wb') as f:
+        #     pickle.dump(expert_data, f, pickle.HIGHEST_PROTOCOL)
 
-        with open(os.path.join('expert_data', args.envname + '.pkl'), 'wb') as f:
-            pickle.dump(expert_data, f, pickle.HIGHEST_PROTOCOL)
 
 if __name__ == '__main__':
     main()
